0e672beebe415dc4ec365638342d865a5f9aad2a
DAVID <nabijonovo23@gmail.com>
2023-04-06 20:30:35 +0300

    project updated

--- app/src/main/java/uz/gita/game2048_bek/ui/main/MainActivity.kt
+++ app/src/main/java/uz/gita/game2048_bek/ui/main/MainActivity.kt
@@ -7,13 +7,16 @@ import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatTextView
 import uz.gita.game2048_bek.R
 import uz.gita.game2048_bek.databinding.ActivityMainBinding
+import uz.gita.game2048_bek.db.MyBase
 import uz.gita.game2048_bek.ui.game.GameActivity
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val myBase by lazy { MyBase.getInstance(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -77,14 +80,17 @@ class MainActivity : AppCompatActivity() {
         dialog.setContentView(R.layout.custom_records_dialog)
         dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
 
-        val btnNo: AppCompatButton = dialog.findViewById(R.id.btnNo)
-        val btnYes: AppCompatButton = dialog.findViewById(R.id.btnYes)
+        val btnClose: AppCompatButton = dialog.findViewById(R.id.btnClose)
+        val btnReset: AppCompatButton = dialog.findViewById(R.id.btnReset)
+        val txtRecord: AppCompatTextView = dialog.findViewById(R.id.txtRecord)
 
-        btnNo.setOnClickListener { dialog.dismiss() }
+        txtRecord.text = "Record \n${myBase.record}"
 
-        btnYes.setOnClickListener {
+        btnClose.setOnClickListener { dialog.dismiss() }
+
+        btnReset.setOnClickListener {
+            myBase.record = 0
             dialog.dismiss()
-            this.finishAffinity()
         }
         dialog.create()
         dialog.show()
